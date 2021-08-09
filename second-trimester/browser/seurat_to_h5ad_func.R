015cbb9020373510c753c1fb9ec66425b06248c6
carmensandoval <carmen.sandoval@ucsf.edu>
2021-08-09 07:57:56 -0700

    Added browser code.

--- second-trimester/browser/seurat_to_h5ad_func.R
+++ second-trimester/browser/seurat_to_h5ad_func.R
@@ -1,18 +1,31 @@
-seurat_to_h5ad <- function(seurat_object_path = NULL, seurat_object = NULL, file_path_out) {
+seurat_to_h5ad <- function(seurat_object_path = NULL, 
+                           seurat_object = NULL, 
+                           file_path_out) {
+  
   if(!is.null(seurat_object_path)) {
+    
     seurat_object <- read_rds(seurat_object_path)
   }
-  SeuratDisk::SaveH5Seurat(object = seurat_object, filename = file_path_out, overwrite = TRUE)
-  SeuratDisk::Convert(source = file_path_out, dest = "h5ad", overwrite = TRUE)
+  
+  SeuratDisk::SaveH5Seurat(object = seurat_object,  # Save H5Seurat adds .h5Seurat to filename
+                           filename = file_path_out, 
+                           overwrite = TRUE)
+  
+  SeuratDisk::Convert(source = paste0(file_path_out, ".h5seurat", 
+                      dest = "h5ad", 
+                      overwrite = TRUE)
+  
   sms("Convert h5ad")
 }
 
 # seurat_to_h5ad(seurat_object = "~/cse-phd/second-trimester/neocortex/data/210322_neocortex_full_v3.2.3_subset_0.01.rds", 
 #               file_path = "~/cse-phd/second-trimester/browser/browser-dbx/data/210323_neocortex_v3.2.3_subset.05.h5seurat")
 
+# /////////////////////////////////////////////////////////////////////////////////////////////////
 
+# For neocortex
 
-cleanMetadata <- function(seurat_object){
+cleanMetadataNeocortex <- function(seurat_object) {
   
   neocortex_annot <- read_tsv("../neocortex/data/tbls/83d19_Neocortex_allindividuals_combinedclusters_v1.txt")
   gw16_annot <- read_tsv("../neocortex/data/tbls/gw16neo_clusteridentity.txt")
@@ -23,7 +36,6 @@ cleanMetadata <- function(seurat_object){
                   str_replace("rg", "radial_glia") %>% 
                   str_replace("-", "_"))
   
-
   
   annot <- rbind(neocortex_annot %>% select(cell_id = cell.name, 
                                             cluster_label = combined.cluster.2),
@@ -31,19 +43,26 @@ cleanMetadata <- function(seurat_object){
                    select(cell_id = cell_name, 
                           cluster_label)) %>%
     mutate(cluster_label = str_remove_all(cluster_label, "_combo2") %>% tolower %>%
-             str_replace("inteneuron", "interneuron") %>%
-             str_replace("endo", "endothelial") %>%
-             str_replace("rg|radial glia", "radial_glia") %>%
-             str_replace("^neuron", "excitatory_neuron") %>%
-             str_replace("^0|other|mixed", "outlier"),
+                           str_replace(c("inteneuron" = "interneuron",
+                                         "endo" = "endothelial",
+                                         "rg|radial glia" = "radial_glia",
+                                         "^neuron" = "excitatory_neuron",
+                                         "^0|other|mixed" = "outlier")),
            cell_type = cluster_label %>% str_remove_all("_[:digit:]+") %>%
-             str_replace("^cr$", "cajal_retzius") 
-    ) %>% left_join(subtypes_annot %>% select(-cell_type), by = "cluster_label") %>%
+                                         str_replace("^cr$", "cajal_retzius")) %>% 
+    left_join(subtypes_annot %>% select(-cell_type), by = "cluster_label") %>%
     mutate(cell_type = case_when(!is.na(annotation ) ~ annotation,
-                                 TRUE ~ cell_type)) %>%
-    select(-annotation)
+                                                TRUE ~ cell_type),
+           # ******
+           lamina = str_replace_all(lamina, c("All" = "all", 
+                                              "VZSVZ" = "VZ_SVZ"))) %>%
+    select(area = "cortical_area", 
+           -annotation, -ncount_rna, -nfeature_rna)
+   
+   # *****
   
   # 457,965 rows
+  
   metadata_tmp <- seurat_object@meta.data %>% rownames_to_column("cell_id")
 
   metadata <- left_join(x = metadata_tmp,
@@ -90,4 +109,40 @@ addReductionRows <- function(reduc_item, new_cells) {
   reduc_item@cell.embeddings <- cell_embeddings
   return(reduc_item)
   
+}
+
+
+# ////////////////////////////////////////////////
+
+cleanMetadataWholeBrain <- function(seurat_object){
+  
+    tmp <- seurat_object@meta.data %>% 
+
+    set_colnames(colnames(.) %>% tolower %>% str_replace_all("[.]", "_")) %>%
+      
+      select(cell_id = cell_name, 
+             age, 
+             individual,
+             brain_region = structure, 
+             area, 
+             lamina, 
+             cell_type = cell_type_v2, 
+             cluster_label = clusterv2, 
+             ngene, numi, percent_mito, percent_ribo) %>%
+
+    mutate_at(.vars = c("cluster_label", "cell_type"), 
+              .funs = ~ tolower(.x) %>% str_replace_all(string = ., 
+                                                    pattern = c("([:digit:]+)[:alpha:]+$" = "\\1",
+                                                             "^neuron" = "excitatory_neuron", 
+                                                             "radialglia|rg" = "radial_glia", 
+                                                             "vascular|endo" = "endothelial"))) %>%
+      
+      mutate(area = case_when(area == "V1" & !(brain_region == "neocortex") ~ brain_region,
+                                                                       TRUE ~ area)) %>%
+      mutate_at(.vars = c("percent_ribo", "percent_mito"),
+                .funs = ~ round(.x, 5)) %>%
+
+      set_rownames(.$"cell_id")
+   
+  return(tmp)
 }
